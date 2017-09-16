70b578094312707d2ac66cb76ee81b9ac5f3c7c4
mw <willia32@hotmail.com>
2017-09-16 11:00:56 -0700

    Complete Part 2 replacing Enum Type with dynamic categories

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,8 +1,6 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,7 +22,8 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -53,11 +52,12 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setType(CheeseType type) {
-        this.type = type;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
+
